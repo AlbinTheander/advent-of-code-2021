14c2c759e9f17de3e1f4012c96330e8a8f7a23cc
Albin Theander <Albin.Theander@jayway.com>
2021-12-02 08:58:25 +0100

    Day 2 - typescript

---
+++ typescript/src/days/day02.ts
@@ -0,0 +1,55 @@
+import { readFileSync } from "fs";
+
+type Command = {
+  cmd: string;
+  arg: number;
+};
+
+export function day02() {
+    const cmds = getData();
+    const p1 = run(cmds);
+    const p2 = runWithAim(cmds);
+    console.log('===== Day 2 =====');
+    console.log('The submarine ends up at ${p1.x},${p1.y} with the product', p1.x * p1.y);
+    console.log('With aiming it ends up at ${p2.x},${p2.y} with the product', p2.x * p2.y);
+}
+
+function run(cmds: Command[]) {
+    let x = 0;
+    let y = 0;
+    for (let {cmd, arg} of cmds) {
+        switch(cmd) {
+            case 'forward': x += arg; break;
+            case 'down': y += arg; break;
+            case 'up': y -= arg; break;
+            default: console.log('Unknown command', cmd);
+        }
+    }
+    return {x, y};
+}
+
+function runWithAim(cmds: Command[]) {
+    let x = 0;
+    let y = 0;
+    let aim = 0;
+    for (let {cmd, arg} of cmds) {
+        switch(cmd) {
+            case 'forward': x = x + arg; y += aim * arg; break;
+            case 'down': aim += arg; break;
+            case 'up': aim -= arg; break;
+            default: console.log('Unknown command', cmd);
+        }
+    }
+    return {x, y};
+}
+
+function getData(): Command[] {
+  return readFileSync("../data/day02.txt", "utf-8")
+    .split("\n")
+    .map((line) => {
+      const [cmd, arg] = line.split(" ");
+      return { cmd, arg: +arg };
+    });
+}
+
+
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,9 @@
 import { day01 } from "./days/day01"
+import { day02 } from "./days/day02";
 
 function main() {
     day01();
+    day02();
 }
 
 
