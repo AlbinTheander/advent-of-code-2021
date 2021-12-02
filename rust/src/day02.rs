db56c7e6934cd325936bcbae64ec408daeb8c840
Albin Theander <Albin.Theander@jayway.com>
2021-12-02 10:39:29 +0100

    Day 2, part 2 - rust

--- rust/src/day02.rs
+++ rust/src/day02.rs
@@ -7,10 +7,12 @@ struct Command {
 
 pub fn solve() {
     let commands = get_commands();
-    let p = navigate_simple(&commands);
+    let p1 = navigate_simple(&commands);
+    let p2 = navigate_with_aim(&commands);
 
     println!("=====  Day 2  =====");
-    println!("The final position is {:?} with the product {}", p, p.0 * p.1);
+    println!("The final position is {:?} with the product {}", p1, p1.0 * p1.1);
+    println!("The final position with aim is {:?} with the product {}", p2, p2.0 * p2.1);
 }
 
 fn navigate_simple(commands: &[Command]) -> (i64, i64) {
@@ -27,6 +29,20 @@ fn navigate_simple(commands: &[Command]) -> (i64, i64) {
     return (x, y);
 }
 
+fn navigate_with_aim(commands: &[Command]) -> (i64, i64) {
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    for cmd in commands {
+         match cmd.operation.as_str() {
+            "forward" => {x += cmd.arg; y += aim * cmd.arg},
+            "down" => aim += cmd.arg,
+            "up" => aim -= cmd.arg,
+            anything => panic!("Unknown command {}", anything)
+         }
+    }
+    return (x, y);
+}
 
 fn get_commands() -> Vec<Command> {
     let data = util::read_data("day02");
