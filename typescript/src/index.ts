0dd8638f6cac27f8e8cf11903e88213ec96180ca
Albin Theander <Albin.Theander@jayway.com>
2021-12-07 06:38:42 +0100

    Day 6 - ugly

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,13 +3,15 @@ import { day02 } from "./days/day02";
 import { day03 } from "./days/day03";
 import { day04 } from "./days/day04";
 import { day05 } from "./days/day05";
+import { day06 } from "./days/day06";
 
 function main() {
-    day01();
-    day02();
-    day03();
-    day04();
-    day05();
+    // day01();
+    // day02();
+    // day03();
+    // day04();
+    // day05();
+    day06();
 }
 
 main()
