f86a1d33721e8ac0588e199ff9a5ea0d5b021fe0
Albin Theander <Albin.Theander@jayway.com>
2021-12-09 19:29:23 +0100

    Clean up Day 7 - ts

--- typescript/src/days/day07.ts
+++ typescript/src/days/day07.ts
@@ -2,46 +2,41 @@ import { readFileSync } from "fs";
 
 export function day07() {
     const crabs = getData();
-    part1(crabs);
-    part2(crabs);
+    const lowFuel = part1(crabs);
+    const highFuel = part2(crabs);
+
+    console.log('===== Day 7  =====');
+    console.log('The minimal fuel when fuel = distance is', lowFuel);
+    console.log('With the real fuel consumption, they need', highFuel);
 }
 
 function part1(crabs) {
-    const min = Math.min(...crabs);
-    const max = Math.max(...crabs);
-    let best = 0;
-    let bestD = Infinity;
-    for(let p = min; p <= max; p++) {
-        const d = crabs.reduce((s, c) => s + Math.abs(c - p), 0);
-        if (d < bestD) {
-            console.log('Better', p, d, bestD);
-            best = p;
-            bestD = d;
-        }
-    }
-    console.log(best, bestD);
+    return findMinimalFuelNeeded(crabs, d => d);
 }
+
 function part2(crabs) {
+    return findMinimalFuelNeeded(crabs, d => d * (d + 1) / 2);
+}
+
+function findMinimalFuelNeeded(crabs: number[], fuelConsumption: (distance: number) => number): number {
     const min = Math.min(...crabs);
     const max = Math.max(...crabs);
-    const fuel = d => d * (d + 1) / 2;
 
     let best = 0;
-    let bestD = Infinity;
+    let bestFuel = Infinity;
     for(let p = min; p <= max; p++) {
-        const d = crabs.reduce((s, c) => s + fuel(Math.abs(c - p)), 0);
-        if (d < bestD) {
-            console.log('Better', p, d, bestD);
+        const d = crabs.reduce((s, c) => s + fuelConsumption(Math.abs(c - p)), 0);
+        if (d < bestFuel) {
             best = p;
-            bestD = d;
+            bestFuel = d;
         }
     }
-    console.log(best, bestD);
+
+    return bestFuel;
 }
 
 
 
 function getData(): number[] {
     return readFileSync('../data/day07.txt', 'utf-8').split(',').map(Number);
-    // return readFileSync('./test.txt', 'utf-8').split(',').map(Number);
 }
